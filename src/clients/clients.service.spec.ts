95cdb1b2c488fa4265018ace417fa97ee4f45337
gftf2011 <gftf2011@gmail.com>
2024-07-19 00:39:56 +0000

    test: should return accessToken

--- src/clients/clients.service.spec.ts
+++ src/clients/clients.service.spec.ts
@@ -150,6 +150,57 @@ describe('Client - Service', () => {
       await expect(promise).rejects.toThrow(new AccountDoesNotExistsError());
     });
 
+    it('should return "accessToken"', async () => {
+      let fakeRepoModule: any;
+
+      /**
+       * Jest Isolation Module
+       *
+       * Module used is a Singleton Class, to create a fresh singleton in each test
+       * it's necessary to isolate the module with jest.isolateModules
+       *
+       * This creates a sandbox where after the test execution the sandbox is destroyed
+       */
+      jest.isolateModules(() => {
+        fakeRepoModule = require('./infra/repositories/clients');
+      });
+
+      const { FakeInMemoryClientsRepository } = fakeRepoModule;
+
+      const stubProvider: ITokenProvider = new StubTokenProvider(
+        [Promise.resolve('access-token')],
+        [],
+      );
+      const fakeRepo: IClientRepository =
+        FakeInMemoryClientsRepository.getInstance() as FakeInMemoryClientsRepository;
+
+      const fullName = 'Adolfo Oliveira';
+      const email = 'test@mail.com';
+      const password = '12345678xX#';
+      const planCreateRechargeTime = Date.now();
+      const planDeleteRechargeTime = Date.now();
+
+      const plan = await fakeRepo.findPlanByTier(PLAN_TYPES.FREE);
+
+      const entity = await ClientEntity.createNew({
+        email,
+        fullName,
+        password,
+        planCreateRechargeTime,
+        planDeleteRechargeTime,
+        planTier: plan.getValue().tier,
+        planId: plan.getValue().id.value,
+      });
+
+      await fakeRepo.save(entity);
+
+      const sut = new ClientsService(fakeRepo, stubProvider);
+
+      const response = await sut.loginToAccount({ email, password });
+
+      expect(response.accessToken).toBe('access-token');
+    });
+
     afterAll(() => {
       /**
        * Most important - restores module to original implementation
